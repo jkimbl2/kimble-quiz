cb08dcdbba3717d727e3bbead8506271eb5d8346
jkimbl2 <jkimbl2@live.spcollege.edu>
2024-03-25 21:35:57 -0400

    fixed the bugs in the test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
   def index
     redirect_to(controller: "users", action: "show", id: session[:user_id]) if !is_admin?
 
-    #Remove this username from the list of users
-    @users = User.where("username <> 'admin3@example.com'").all
+    #No longer remove this username from the list of users
+    @users = User.all
   end
 
   # GET /users/1 or /users/1.json
